ec1d6150b35f7e187b0c097d81535c483a061fd7
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-01-09 13:44:58 -0300

    feat: adicionando o exercício da primeira Sessão do curso

---
+++ 1_INTRO/hello-world/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>
+        Hello, World!
+      </h1>
+      <h2>
+        Funcionou o Emmet perfeitamente!
+      </h2>
+      <button id='botao'>
+        Pronta para seguir em frente?
+      </button>
+    </>
+  )
+}
+
+export default App
