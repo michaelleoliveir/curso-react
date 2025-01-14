ee354d3b998174d6e7a3ed1902d284de89735816
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-01-14 11:05:54 -0300

    feat: quase terminando a Seção 3

--- 3_AVANCANDO_NO_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_NO_REACT/avancando/src/App.jsx
@@ -5,8 +5,14 @@ import ListRender from './components/ListRender'
 import Conditional from './components/Conditional'
 import ShowUserName from './components/ShowUserName'
 import CarDetails from './components/CarDetails'
+import Fragment from './components/Fragment'
+import Container from './components/Container'
+import ExecuteFunction from './components/ExecuteFunction'
+import Message from './components/Message'
+import ChangeMessageState from './components/ChangeMessageState'
 
 import City from './assets/city.jpg'
+import { useState } from 'react'
 
 function App() {
 
@@ -15,6 +21,17 @@ function App() {
     { id: 2, brand: "KIA", color: "Branco", novo: false, km: 32423 },
     { id: 3, brand: "Renault", color: "Azul", novo: false, km: 234 }
   ]
+
+  function showMessage() {
+    console.log("Evento do componente pai")
+  }
+
+  const [ message, setMessage ] = useState("");
+
+  // função que altera o estado
+  const handleMessage = (msg) => {
+    setMessage(msg)
+  }
 
   return (
     <div>
@@ -43,12 +60,29 @@ function App() {
 
       { cars.map((car) => (
         <CarDetails 
+          key={car.id}
           brand={car.brand} 
           color={car.color} 
           km={car.km} 
           novo={car.novo}
           />
       )) }
+
+      <Fragment propFragment="teste"/>
+
+      <Container myValue="testing">
+        <p>Este é o conteúdo</p>
+      </Container>
+
+      <Container myValue="testing 2">
+        <h5>Outro teste</h5>
+      </Container>
+
+      <ExecuteFunction myFunction={showMessage}/>
+
+      <Message msg={message}/>
+
+      <ChangeMessageState handleMessage={handleMessage}/>
     </div>
   )
 }
