d1a8288394afd1b8fae79f34074794b05ce3e2cc
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-01-15 14:40:13 -0300

    feat: terminando a Seção de CSS

--- 4_CSS_REACT/css/src/App.jsx
+++ 4_CSS_REACT/css/src/App.jsx
@@ -1,16 +1,36 @@
+import { useState } from 'react';
+
 import './App.css'
 
 import MyComponent from './components/MyComponent'
+import Title from './components/Title';
 
 function App() {
+
+  const n = 15;
+  const [name] = useState("Matheus");
+  const redTitle = true;
   
   return (
     <div className='App'>
       <h1>React com CSS</h1>
+
       <MyComponent />
       <p>Este parágrafo é do App.jsx</p>
 
+      {/* estilização inline */}
       <p style={{color: "blue", padding: "25px", borderTop: "6px solid red"}}>Este elemento foi estilizado de forma Inline</p>
+
+      {/* estilizacao utilizando o if ternário */}
+      <h2 style={n < 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS Dinâmico</h2>
+      <h2 style={n > 10 ? ({color: "purple"}) : ({color: "pink"})}>CSS Dinâmico</h2>
+      <h2 style={name === "Matheus" ? ({color: "green", backgroundColor: "#000"}) : null}>CSS Dinâmico</h2>
+
+      {/* estilização com classe dinâmica */}
+      <h2 className={redTitle ? "red-title" : "title"}>Este título vai ter classe dinâmica</h2>
+
+      {/* css modules */}
+      <Title />
     </div>
   )
 }
