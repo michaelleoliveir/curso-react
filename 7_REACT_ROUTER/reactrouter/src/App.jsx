6797f6659dd5f424f1de6bfa42c57bd2d8f10ae2
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-01-29 20:52:27 -0300

    feat: terminando a Seção de Routes

--- 7_REACT_ROUTER/reactrouter/src/App.jsx
+++ 7_REACT_ROUTER/reactrouter/src/App.jsx
@@ -1,16 +1,19 @@
 import './App.css';
 
 // config react router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 // pages
 import  Home from "./pages/Home";
 import  About  from "./pages/About";
 import Products from './pages/Products';
 import Info from './pages/Info';
+import NotFound from './pages/NotFound';
+import Search from './pages/Search';
 
 // components
 import Navbar from './components/Navbar';
+import SearchForm from './components/SearchForm';
 
 function App() {
 
@@ -21,12 +24,17 @@ function App() {
         {/* inserindo o componente de navbar dentro do App */}
         <Navbar />
 
+        <SearchForm />
+
         {/* inserindo as rotas presentes no projeto */}
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/about' element={<About />}/>
           <Route path='/products/:id' element={<Products />}/>
           <Route path='/products/:id/info' element={<Info />}/>
+          <Route path='/search' element={<Search />}/>
+          <Route path="*" element={<NotFound />}/>
+          <Route path="/company" element={<Navigate to="/about" />}/>
         </Routes>
       </BrowserRouter>
     </div>
--- 7_REACT_ROUTER/reactrouter/src/components/Navbar.jsx
+++ 7_REACT_ROUTER/reactrouter/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import './Navbar.css'
 
-import { Link } from 'react-router'
+import { Link, NavLink } from 'react-router'
 
 const Navbar = () => {
     return (
         <nav>
-            <Link to='/'>Home</Link>
-            <Link to='/about'>About</Link>
+            {/* <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link> */}
+
+            <NavLink to="/" className={(isActive) => (isActive ? "esta-ativo" : "nao-ativo")}>Home</NavLink>
+            <NavLink to="/about">Sobre</NavLink>
         </nav>
     )
 }
